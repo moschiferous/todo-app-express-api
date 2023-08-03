a9f37c1f0addf562c8b07ad145026b4c7e16837d
BagoesPantera <bagoespantera01@gmail.com>
2023-08-03 17:38:58 +0800

    init prisma

---
+++ README.md
@@ -0,0 +1,17 @@
+# Express TypeScript
+
+### Setup
+```sh
+npm install
+```
+```sh
+npx prisma generate
+```
+```sh
+npx prisma migrate dev
+```
+
+### Run
+```sh
+npm run dev
+```
---
+++ prisma/migrations/20230803092757_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
